f5180800004ea40f9640362ee5b0d9a2e0553748
Chad <chadlada@gmail.com>
2022-02-03 18:12:55 -0500

    Added base controller for Pets

---
+++ Migrations/20220203230247_AddPlaytimes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TamagotchiAPI.Migrations
+{
+    public partial class AddPlaytimes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Playtimes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    When = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    PetId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Playtimes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Playtimes");
+        }
+    }
+}
